e49af5542794ca54752664df105d0574d198b8aa
Aleksey Vitebskiy <aleksey.vitebskiy@digitalglobe.com>
2017-06-26 23:47:41 -0400

    Fixed spacing in description.

--- src/main.cpp
+++ src/main.cpp
@@ -171,9 +171,9 @@ po::options_description buildVisibleOptions()
         "Version: " GBDXM_VERSION_STRING "\n\n"
         "Usage: gbdxm <action> [options] [gbdxm file]\n\n"
         "Actions:\n"
-        "  help\t\t\t Show this help message.\n"
-        "  show\t\t\t Show package metadata.\n"
-        "  pack\t\t\t Pack a model into a GBDX package.\n"
+        "  help  \t\t Show this help message.\n"
+        "  show  \t\t Show package metadata.\n"
+        "  pack  \t\t Pack a model into a GBDX package.\n"
         "  unpack\t\t Unpack a GBDX package and output the original model.\n\n"
         "General Options"
     );
